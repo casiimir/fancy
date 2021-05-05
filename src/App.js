01780c33cdd83953b557057b75bd64a5ce779e15
casiimir <cp.ciancimino@gmail.com>
2021-05-05 20:05:41 +0200

    feature: add post component and refine relative scss

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import styles from "./App.module.scss";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
+import PostContainer from "./components/PostContainer";
+
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
       <Hero />
+      <main>
+        <PostContainer />
+      </main>
     </div>
   );
 }
