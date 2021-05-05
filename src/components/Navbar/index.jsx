3af1bf7773fd9043bda92df1fd5f8633b9afeb60
casiimir <cp.ciancimino@gmail.com>
2021-05-05 16:09:48 +0200

    feature: add scss mixins and variables

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./index.module.scss";
+
+const Navbar = () => {
+  return (
+    <div className={styles.Navbar}>
+      <h1>Title</h1>
+      <h3>Cross</h3>
+    </div>
+  );
+};
+
+export default Navbar;
