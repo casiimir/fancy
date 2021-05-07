45697b0b25532bda6215ba53e6ccf4ea0441ea49
casiimir <cp.ciancimino@gmail.com>
2021-05-07 23:13:33 +0200

    feature: add main page scrolling logics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import styles from "./App.module.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -8,14 +8,29 @@ import Modal from "./components/Modal";
 import ExtGallery from "./components/ExtGallery";
 
 function App() {
+  // Modal and Gallery logics
   const [isModalVisible, setModalVisibile] = useState(false);
   const [isGalleryVisibile, setGalleryVisible] = useState(false);
+
+  // Scrolling logic
+  const [offsetY, setOffsetY] = useState(0);
+  const [fullWidthMain, setFullWidthMan] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => setOffsetY(window.scrollY));
+
+    if (offsetY >= 526) setFullWidthMan(true);
+    else setFullWidthMan(false);
+  });
 
   return (
     <div className={styles.App}>
       <Navbar onHandle={setModalVisibile} />
       <Hero />
-      <MainBody onGalleryHandle={setGalleryVisible} />
+      <MainBody
+        onGalleryHandle={setGalleryVisible}
+        fullWidthMain={fullWidthMain}
+      />
       {isModalVisible && (
         <Modal onHandle={setModalVisibile}>
           <ul>
